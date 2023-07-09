5cbd0eb0fc6b818ff995d2f3c03a399ac4c97772
Eddy518 <edmwangi2@gmail.com>
2023-07-09 16:54:07 +0300

    Fix conditional statement

--- index.js
+++ index.js
@@ -1,53 +1,28 @@
 //array called ComputerChoice that will store  rock,paper,scissors
-let computerChoice = ["Rock", "Paper", "Scissors"];
+let computerChoice = ["rock", "paper", "scissors"];
 //function called getComputerChoice that will return either rock,paper or scissors
 function getComputerChoice() {
   return computerChoice[Math.floor(Math.random() * computerChoice.length)];
 }
 //getComputerChoice();
+
 //function to play single round of Rock Paper Scissors
 function playRound(playerSelection, computerSelection) {
-  let result = "";
-  if (computerSelection == "Rock") {
-    if (playerSelection == "Scissors") {
-      result = "You lost! COM chose Rock";
-      return result;
-    } else if (playerSelection == "Paper") {
-      result = "Good guess you win! COM chose Rock!!";
-      return result;
-    } else {
-      result = "You both selected Rock, the game is a draw!";
-      return result;
-    }
-  }
-
-  if (computerSelection == "Paper") {
-    if (playerSelection == "Scissors") {
-      result = "Thats a win! COM chose Paper";
-      return result;
-    } else if (playerSelection == "Rock") {
-      result = "You lost ! COM chose Paper";
-      return result;
-    } else {
-      result = "You both selected Paper, the game is a draw!";
-      return result;
-    }
-  }
-
-  if (computerSelection == "Scissors") {
-    if (playerSelection == "Rock") {
-      result = "Win for you COM chose Scissors";
-      return result;
-    } else if (playerSelection == "Paper") {
-      result = "You lost! COM chose Scissors!!";
-      return result;
-    } else {
-      result = "You both selected Scissors, the game is a draw!";
-      return result;
-    }
+  if (computerSelection == playerSelection) {
+    return `The game is a draw you${playerSelection} COM ${computerSelection}`;
+  } else if (
+    (computerSelection == "rock" && playerSelection == "paper") ||
+    (computerSelection == "paper" && playerSelection == "scissors") ||
+    (computerSelection == "scissors" && playerSelection == "rock")
+  ) {
+    return `You won! COM: ${computerSelection} vs you: ${playerSelection}`;
+  } else {
+    return `You lose! COM: ${computerSelection} vs you: ${playerSelection}`;
   }
 }
+
+
 //get player input
-let playerSelection = prompt("Enter Rock, Paper or Scissors: ");
+let playerSelection = prompt("Enter Rock, Paper or Scissors: ").toLowerCase();
 computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
