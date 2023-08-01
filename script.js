1f97165edbd47b0c52a87c6bb9d83d2da3f92128
Eddy518 <edmwangi2@gmail.com>
2023-08-01 21:27:16 +0300

    DOM setup

--- script.js
+++ script.js
@@ -7,26 +7,7 @@ function getComputerChoice() {
   return computerChoice[Math.floor(Math.random() * computerChoice.length)];
 }
 
-
-function game(){
-  
-  for (let i=1;i<=5;i++){
-    let playerSelection = prompt("Enter Rock, Paper or Scissors: ").toLowerCase();
-    computerSelection = getComputerChoice();
-    let result = playRound(playerSelection,computerSelection);
-    console.log(result);
-  }
-  if (playerScore > computerScore) {
-    console.log("You won in 5 rounds");
-  } else if(playerScore<computerScore){
-    console.log("You lost in 5 rounds");
-  }
-  else{
-    console.log("Overall is a draw");
-  }
-}
 function playRound(playerSelection, computerSelection) {
-  
   if (computerSelection === playerSelection) {
     return `The game is a draw you: ${playerSelection} COM: ${computerSelection}`;
   } else if (
@@ -34,14 +15,21 @@ function playRound(playerSelection, computerSelection) {
     (computerSelection === "paper" && playerSelection === "scissors") ||
     (computerSelection === "scissors" && playerSelection === "rock")
   ) {
-      playerScore++;
-      return `You won! COM: ${computerSelection} vs you: ${playerSelection}`;
+    playerScore++;
+    return `You won! COM: ${computerSelection} vs you: ${playerSelection}`;
   } else {
-     computerScore++;
-     return `You lose! COM: ${computerSelection} vs you: ${playerSelection}`;
-  }   
-} 
+    computerScore++;
+    return `You lose! COM: ${computerSelection} vs you: ${playerSelection}`;
+  }
+}
 
-game();
 console.log(playerScore);
 console.log(computerScore);
+
+//-----------DOM----------------//
+const btn = document.querySelectorAll(".btn");
+btn.forEach((button) => {
+  button.addEventListener("click", () => {
+    alert("Hello");
+  });
+});
