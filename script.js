a0e2670d2e3d62f09442f63d45320fd92775628b
Eddy518 <edmwangi2@gmail.com>
2023-08-06 11:43:58 +0300

    Add text to show what player selections against computer selections

--- script.js
+++ script.js
@@ -6,16 +6,19 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
   if (computerSelection === playerSelection) {
+    runningScoreDisplay.textContent = `Draw!! You chose ${playerSelection} and Computer chose ${computerSelection}`;
   } else if (
     (computerSelection === "rock" && playerSelection === "paper") ||
     (computerSelection === "paper" && playerSelection === "scissors") ||
     (computerSelection === "scissors" && playerSelection === "rock")
   ) {
+    runningScoreDisplay.textContent = `You win!! You chose ${playerSelection} and Computer chose ${computerSelection}`;
     console.log(`playerScore original: ${playerScore}`);
     playerScore++;
     playerScoreDisplay.textContent = playerScore;
     console.log(`playerScore after: ${playerScore}`);
   } else {
+    runningScoreDisplay.textContent = `You lose!! You chose ${playerSelection} and Computer chose ${computerSelection}`;
     console.log(`computerScore original: ${computerScore}`);
     computerScore++;
     computerScoreDisplay.textContent = computerScore;
@@ -26,6 +29,7 @@ function playRound(playerSelection, computerSelection) {
 const selections = document.querySelectorAll(".selection button");
 const playerScoreDisplay = document.querySelector("#player-result");
 const computerScoreDisplay = document.querySelector("#computer-result");
+const runningScoreDisplay = document.querySelector("#running-score");
 let playerScore = 0;
 let computerScore = 0;
 
